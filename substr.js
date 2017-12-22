324b32d135d763c275fe70b01ecb00828b3802bf
Vasyl Stokolosa <vasyl.stokolosa@synapse.com>
2017-12-22 17:00:32 +0200

    JSDoc

--- substr.js
+++ substr.js
@@ -11,7 +11,7 @@ var isString = require('lodash/isString');
  * @category String
  * @param {String} string -  The string to extract.
  * @param {Number} start -  The position where to start the extraction. First character is at index 0.
- * @param {Number=} length -  The number of characters to extract. If omitted, it extracts the rest of the string.
+ * @param {Number?} length -  Optional. The number of characters to extract. If omitted, it extracts the rest of the string.
  * @returns {String} Returns extract part of a string.
  * @example
  *
