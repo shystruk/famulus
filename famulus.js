82c96d77345ecf7e1bc2ef76f3997aa1aaf0f997
shystruk <wystruk@ukr.net>
2018-03-10 17:51:21 +0200

    Export in index

--- famulus.js
+++ famulus.js
@@ -1,9 +1,11 @@
 var substr = require('./substr');
 var sortAndAddFirstElement = require('./sortAndAddFirstElement');
 var dateDifference = require('./dateDifference');
+var isValuesUnique = require('./isValuesUnique');
 
 module.exports = {
     substr: substr,
     sortAndAddFirstElement: sortAndAddFirstElement,
-    dateDifference: dateDifference
+    dateDifference: dateDifference,
+    isValuesUnique: isValuesUnique
 };
